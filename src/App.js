b23936e4cdea4f61b2c008ae008341eb692282d0
Sapinderjit <er.sapinder.cs@gmail.com>
2023-06-30 12:46:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App=() =>{
   const[user,setUser]=useState('');
   const[email,setEmail]=useState('');
   const[password,setPassword]=useState('');
-  const[emailError,setEmailError]=useState('1');
+  const[emailError,setEmailError]=useState('');
   const[passwordError,setPasswordError]=useState('');
   const[hasAccount,setHasAccount]=useState(false);
 
@@ -96,7 +96,10 @@ useEffect(()=>
 }, [] 
 );
 
+
+
   return (
+   
     <div className="App">
 
       {user ? (<Hero handleLogout={handleLogout} />) : 
@@ -117,7 +120,11 @@ useEffect(()=>
       )}
  
     </div>
+    
+    
   );
+  
+  
 };
 
 
--- src/Hero.js
+++ src/Hero.js
@@ -1,4 +1,50 @@
 import React from "react";
+import Chatbot from 'react-simple-chatbot';
+import {Segment} from 'semantic-ui-react';
+
+
+const steps=[
+    {
+      id:'Hi',
+      message:'Welcome to my React Website',
+      trigger:'Ask Name'
+  
+    },
+    {
+      id:'Ask Name',
+      message:'Please enter your name',
+      trigger:'waiting1'
+    },
+    {
+      id:'waiting1',
+      user:true,
+      trigger:'Name'
+    },
+    {
+      id:'Name',
+      message:'Hi {previousValue}, please select your issue',
+      trigger:'issues'
+  
+    },
+    
+    
+    {
+      id:'issues',
+      options:[{value:"react", label:"react", trigger:"react"},
+               {value:"Angular", label:"Angular", trigger:"Angular"}]
+               
+    },
+    {
+      id:'react',
+      message:'Thanks for telling your react issue',
+      end:true
+    },
+    {
+      id:'Angular',
+      message:'Thanks for telling your Angular issue',
+      end:true
+    }
+  ]
 
 const Hero=({handleLogout})=>
 {
@@ -11,8 +57,17 @@ const Hero=({handleLogout})=>
         </nav>
         <body>
             <center><h1>Welcome to the Home Page</h1></center>
+            
         </body>
+    
+    
+    <Segment floated="right">
+      <Chatbot steps={steps} />
+      </Segment>
+
     </section>
+   
+    
     )
 };
 
